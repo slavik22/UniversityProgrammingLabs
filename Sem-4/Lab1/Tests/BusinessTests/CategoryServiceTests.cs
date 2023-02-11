fb3f8578ec7db0be54d62ddbb2349dfd537eae32
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-02-11 15:28:57 +0200

    Buisness layer CategoryService, CommentService and TagService tests added

---
+++ Sem-4/Lab1/Tests/BusinessTests/CategoryServiceTests.cs
@@ -0,0 +1,168 @@
+﻿// ***********************************************************************
+// Assembly         : Tests
+// Author           : Slava
+// Created          : 12-11-2022
+//
+// Last Modified By : Slava
+// Last Modified On : 12-11-2022
+// ***********************************************************************
+// <copyright file="CategoryServiceTests.cs" company="Tests">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using BuisnessLogicLayer.Models;
+using BuisnessLogicLayer.Services;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests.BusinessTests;
+
+/// <summary>
+/// Class CategoryServiceTest.
+/// </summary>
+public class CategoryServiceTest
+{
+    /// <summary>
+    /// Defines the test method CategoryService_GetAll_ReturnsAllCategories.
+    /// </summary>
+    [Test]
+    public void CategoryService_GetAll_ReturnsAllCategories()
+    {
+        //arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+        mockUnitOfWork
+            .Setup(x => x.CategoryRepository.GetAll(null,"",null))
+            .Returns(GetTestCategories.AsEnumerable());
+        
+        var categoryService = new CategoryService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+
+        //act
+        var actual =  categoryService.GetAll();
+
+        //assert
+        actual.Should().BeEquivalentTo(GetTestCategoryModels);
+    }
+
+
+    /// <summary>
+    /// Defines the test method CategoryService_GetById_ReturnsCategoryModel.
+    /// </summary>
+    [Test]
+    public void CategoryService_GetById_ReturnsCategoryModel()
+    {
+        //arrange
+        var expected = GetTestCategoryModels.First();
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+        mockUnitOfWork
+            .Setup(m => m.CategoryRepository.GetById(It.IsAny<int>(),""))
+            .Returns(GetTestCategories.First());
+
+        var categoryService = new CategoryService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+
+        //act
+        var actual =  categoryService.GetById(1);
+
+        //assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    /// <summary>
+    /// Defines the test method CategoryService_Add_AddsModel.
+    /// </summary>
+    [Test]
+    public void CategoryService_Add_AddsModel()
+    {
+        //arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        mockUnitOfWork.Setup(m => m.CategoryRepository.Add(It.IsAny<Category>()));
+
+        var categoryService = new CategoryService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+        var category = GetTestCategoryModels.First();
+
+        //act
+         categoryService.Add(category);
+
+        //assert
+        mockUnitOfWork.Verify(x => x.CategoryRepository.Add(It.Is<Category>(t =>
+            t.Id == category.Id && t.Title == category.Title)), Times.Once);
+        mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once);
+    }
+
+    /// <summary>
+    /// Defines the test method CategoryService_Delete_DeletesCategory.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(100)]
+    public void CategoryService_Delete_DeletesCategory(int id)
+    {
+        //arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        mockUnitOfWork.Setup(m => m.CategoryRepository.Delete(It.IsAny<int>()));
+        var categoryService = new CategoryService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+
+        //act
+        categoryService.Delete(id);
+
+        //assert
+        mockUnitOfWork.Verify(x => x.CategoryRepository.Delete(id), Times.Once());
+        mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once());
+    }
+
+    /// <summary>
+    /// Defines the test method CategoryService_Update_UpdatesCategory.
+    /// </summary>
+    [Test]
+    public void CategoryService_UpdateAsync_UpdatesCategory()
+    {
+        //arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        mockUnitOfWork.Setup(m => m.CategoryRepository.Update(It.IsAny<Category>()));
+
+        var categoryService = new CategoryService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+        var category = GetTestCategoryModels.First();
+
+        //act
+        categoryService.Update(category);
+
+        //assert
+        mockUnitOfWork.Verify(x => x.CategoryRepository.Update(It.Is<Category>(t =>
+            t.Id == category.Id && t.Title == category.Title )), Times.Once);
+        mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once);
+    }
+
+
+    #region TestData
+
+    /// <summary>
+    /// Gets the get test categories.
+    /// </summary>
+    /// <value>The get test categories.</value>
+    private List<Category> GetTestCategories =>
+        new()
+        {
+            new Category { Id = 1, Title = "Programing" },
+            new Category { Id = 2, Title = "Music" }
+        };
+
+    /// <summary>
+    /// Gets the get test category models.
+    /// </summary>
+    /// <value>The get test category models.</value>
+    private List<CategoryModel> GetTestCategoryModels =>
+        new()
+        {
+            new CategoryModel { Id = 1, Title = "Programing" },
+            new CategoryModel { Id = 2, Title = "Music" }
+        };
+    
+    #endregion
+}
+
---
+++ Sem-4/Lab1/Tests/BusinessTests/CommentServiceTests.cs
@@ -0,0 +1,195 @@
+﻿// ***********************************************************************
+// Assembly         : Tests
+// Author           : Slava
+// Created          : 12-11-2022
+//
+// Last Modified By : Slava
+// Last Modified On : 12-11-2022
+// ***********************************************************************
+// <copyright file="CommentServiceTests.cs" company="Tests">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Linq.Expressions;
+using BuisnessLogicLayer.Models;
+using BuisnessLogicLayer.Services;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests.BusinessTests;
+
+/// <summary>
+/// Class CommentServiceTest.
+/// </summary>
+public class CommentServiceTest
+{
+    /// <summary>
+    /// Defines the test method CommentService_GetAll_ReturnsAllComments.
+    /// </summary>
+    [Test]
+    public void  CommentService_GetAll_ReturnsAllComments()
+    {
+        //arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+        mockUnitOfWork
+            .Setup(x => x.CommentRepository.GetAll(null,"",null))
+            .Returns(GetTestComments.AsEnumerable());
+        
+        var commentService = new CommentService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+
+        //act
+        var actual =  commentService.GetAll();
+
+        //assert
+        actual.Should().BeEquivalentTo(GetTestCommentModels);
+    }
+
+    /// <summary>
+    /// Defines the test method PostService_GetPostComments_ReturnsPostComments.
+    /// </summary>
+    /// <param name="postId">The post identifier.</param>
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(100)]
+    public void  PostService_GetPostComments_ReturnsPostComments(int postId)
+    {
+        //arrange
+        var expected = GetTestCommentModels.Where(x => x.PostId == postId);
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+        mockUnitOfWork
+            .Setup(x => x.CommentRepository.GetAll(It.IsAny<Expression<Func<Comment,bool>>>(),"",null))
+            .Returns(GetTestComments.Where(x => x.PostId == postId));
+        
+        var commentService = new CommentService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+
+        //act
+        var actual =  commentService.GetPostComments(postId);
+
+        //assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+
+    /// <summary>
+    /// Defines the test method CommentService_GetById_ReturnsCommentModel.
+    /// </summary>
+    [Test]
+    public void  CommentService_GetById_ReturnsCommentModel()
+    {
+        //arrange
+        var expected = GetTestCommentModels.First();
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+
+        mockUnitOfWork
+            .Setup(m => m.CommentRepository.GetById(It.IsAny<int>(),""))
+            .Returns(GetTestComments.First());
+
+        var commentService = new CommentService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+
+        //act
+        var actual =  commentService.GetById(1);
+
+        //assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    /// <summary>
+    /// Defines the test method CommentService_Add_AddsModel.
+    /// </summary>
+    [Test]
+    public void  CommentService_Add_AddsModel()
+    {
+        //arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        mockUnitOfWork.Setup(m => m.CommentRepository.Add(It.IsAny<Comment>()));
+
+        var commentService = new CommentService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+        var comment = GetTestCommentModels.First();
+
+        //act
+         commentService.Add(comment);
+
+        //assert
+        mockUnitOfWork.Verify(x => x.CommentRepository.Add(It.Is<Comment>(t =>
+            t.Id == comment.Id && t.Title == comment.Title && t.Content == comment.Content && t.PostId == comment.PostId)), Times.Once);
+        mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once);
+    }
+
+    /// <summary>
+    /// Defines the test method CommentService_Delete_DeletesComment.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(100)]
+    public void  CommentService_Delete_DeletesComment(int id)
+    {
+        //arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        mockUnitOfWork.Setup(m => m.CommentRepository.Delete(It.IsAny<int>()));
+        var commentService = new CommentService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+
+        //act
+         commentService.Delete(id);
+
+        //assert
+        mockUnitOfWork.Verify(x => x.CommentRepository.Delete(id), Times.Once());
+        mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once());
+    }
+
+    /// <summary>
+    /// Defines the test method CommentService_Update_UpdatesComment.
+    /// </summary>
+    [Test]
+    public void  CommentService_Update_UpdatesComment()
+    {
+        //arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        mockUnitOfWork.Setup(m => m.CommentRepository.Update(It.IsAny<Comment>()));
+
+        var commentService = new CommentService(mockUnitOfWork.Object, UnitTestHelper.CreateMapperProfile());
+        var comment = GetTestCommentModels.First();
+
+        //act
+         commentService.Update(comment);
+
+        //assert
+        mockUnitOfWork.Verify(x => x.CommentRepository.Update(It.Is<Comment>(t =>
+            t.Id == comment.Id && t.Title == comment.Title && t.Content == comment.Content && t.PostId == comment.PostId )), Times.Once);
+        mockUnitOfWork.Verify(x => x.SaveAsync(), Times.Once);
+    }
+
+
+    #region TestData
+
+    /// <summary>
+    /// Gets the get test comments.
+    /// </summary>
+    /// <value>The get test comments.</value>
+    private List<Comment> GetTestComments =>
+        new()
+        {
+            new Comment {Id = 1, Title = "Hello from Slava", Content = "I completely agree with author's opinion", PostId = 1},
+            new Comment {Id = 2, Title = "Hello from Tom", Content = "I completely agree with author's opinion", PostId = 2}
+        };
+
+    /// <summary>
+    /// Gets the get test comment models.
+    /// </summary>
+    /// <value>The get test comment models.</value>
+    private List<CommentModel> GetTestCommentModels =>
+        new()
+        {
+            new CommentModel {Id = 1, Title = "Hello from Slava", Content = "I completely agree with author's opinion", PostId = 1},
+            new CommentModel {Id = 2, Title = "Hello from Tom", Content = "I completely agree with author's opinion", PostId = 2}
+        };
+    
+    #endregion
+}
+
