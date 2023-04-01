2bcccc7ec20cd96b56b642d25cb8d1ec0b57d334
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-04-01 11:54:12 +0300

    Command pattern is implemented

--- Sem-4/Lab2/BuisnessLogicLayer/Services/NotificationService.cs
+++ Sem-4/Lab2/BuisnessLogicLayer/Services/NotificationService.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using BuisnessLogicLayer.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BuisnessLogicLayer.Services
 {
-    internal interface NotificationService
+    public class NotificationService : INotificationService
     {
+        public Task AddWebSocket(int userId, WebSocket webSocket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveWebSocket(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendNotificationToUser(int userId, string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
