b4226ee0db73ecbf3096365093b428781294caee
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-05-09 20:57:31 +0300

    fixed bug

--- Sem-4/project/old/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  api: {
-    protocol: 'http',
-    hostname: 'localhost',
-    port: '8081',
-    context: 'api'
-  }
-};
--- Sem-4/project/old/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  api: {
-    protocol: 'http',
-    hostname: 'localhost',
-    port: '8081',
-    context: 'api'
-  }
-};
