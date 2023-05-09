1de392b08350c80cdfb5698082c359807368c07a
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-05-09 20:51:28 +0300

    frontend init

---
+++ Sem-4/project/old/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './shared/component/login/login.component';
+import { RegisterComponent } from './shared/component/register/register.component';
+import { PageNotFoundComponent } from './shared/component/page-not-found/page-not-found.component';
+import { AuthGuard } from './shared/guard/auth.guard';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/post' , pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'post', loadChildren: () => import('./post/post.module').then(m => m.PostModule), canActivate: [AuthGuard] },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
