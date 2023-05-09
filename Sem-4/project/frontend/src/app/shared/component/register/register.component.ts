62912bc4acd0531fb904794c3fa845091975b56e
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-05-09 20:55:27 +0300

    register implemented

---
+++ Sem-4/project/frontend/src/app/shared/component/register/register.component.ts
@@ -0,0 +1,45 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  public authForm!: FormGroup;
+  public hasError: boolean = false;
+  public errorMessage: string = '';
+
+  constructor(private _auth: AuthService,
+    private _formBuilder: FormBuilder,
+    private _router: Router) {
+    this.authForm = this._buildAuthForm();
+  }
+
+  public onSubmit(): void {
+    const { username, email, password } = this.authForm.value;
+    this.hasError = false;
+
+    this._auth.register(username, email, password).subscribe(
+      data => {
+        this._router.navigateByUrl('/post');
+      },
+      err => {
+        this.hasError = true;
+        this.errorMessage = err.error.message;
+      }
+    );
+  }
+
+  private _buildAuthForm(): FormGroup {
+    return this._formBuilder.group({
+      username: [null, Validators.required],
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+    })
+  }
+}
